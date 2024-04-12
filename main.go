3fb3950f92fb36fcdcb157de4e9b9fb808483e59
maoadrian12 <maoadrian12@gmail.com>
2024-04-12 14:59:20 -0400

    also added go to support getting levels

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -316,8 +317,8 @@ func getWkst(w http.ResponseWriter, r *http.Request) {
 			Message: "error getting wksts",
 		})
 	} else {
-		fmt.Println(reading_wkst)
-		fmt.Println(math_wkst)
+		//fmt.Println(reading_wkst)
+		//fmt.Println(math_wkst)
 		utils.JsonResponse(w, models.BaseResult{
 			Result:  true,
 			Message: fmt.Sprintf("%v %v", reading_wkst, math_wkst),
@@ -362,6 +363,74 @@ func complete(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func nextLevel(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getLevels(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("In this function!\n")
+	defer r.Body.Close()
+	buf := new(strings.Builder)
+	io.Copy(buf, r.Body)
+	var parsedData map[string]string
+	err := json.Unmarshal([]byte(buf.String()), &parsedData)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+	// Extract variables from the parsed data
+	studentName := parsedData["name"]
+	parentName := parsedData["parent_username"]
+
+	var reading_wkst models.Completes
+	var math_wkst models.Completes
+	var reading_wksts []string
+	var math_wksts []string
+	var rows *sql.Rows
+	rows, err = database.Db.Query("SELECT DISTINCT wkst_lvl FROM completes WHERE student_name = $1 AND parent_username = $2 AND program_name = 'READING'",
+		studentName, parentName)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		if err := rows.Scan(&reading_wkst.Wkst_lvl); err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println("reading ", reading_wkst.Wkst_lvl)
+			reading_wksts = append(reading_wksts, reading_wkst.Wkst_lvl)
+		}
+	}
+	rows, err = database.Db.Query("SELECT DISTINCT wkst_lvl FROM completes WHERE student_name = $1 AND parent_username = $2 AND program_name = 'MATH'",
+		studentName, parentName)
+	if err != nil {
+		fmt.Println("Error querying from completes:", err)
+		return
+	}
+	for rows.Next() {
+		if err := rows.Scan(&math_wkst.Wkst_lvl); err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println("math ", math_wkst.Wkst_lvl)
+			math_wksts = append(math_wksts, math_wkst.Wkst_lvl)
+		}
+	}
+	if err != nil {
+		fmt.Println("Error querying from completes:", err)
+		utils.JsonResponse(w, models.BaseResult{
+			Result:  false,
+			Message: "error getting wksts",
+		})
+	} else {
+		fmt.Printf("reading worksheets are: %v\n", reading_wksts)
+		fmt.Printf("math worksheets are: %v\n", math_wksts)
+		utils.JsonResponse(w, models.BaseResult{
+			Result:  true,
+			Message: fmt.Sprintf("%v %v", reading_wksts, math_wksts),
+		})
+	}
+}
+
 func handleRequests() {
 	myrouter := mux.NewRouter().StrictSlash(false)
 	myrouter.HandleFunc("/", allParents).Methods("GET")
@@ -376,7 +445,9 @@ func handleRequests() {
 	myrouter.HandleFunc("/checkstudent", checkStudent).Methods("POST")
 	myrouter.HandleFunc("/getpages", getPages).Methods("POST")
 	myrouter.HandleFunc("/getinfo", getWkst).Methods("POST")
+	myrouter.HandleFunc("/getlevels", getLevels).Methods("POST")
 	myrouter.HandleFunc("/complete", complete).Methods("POST")
+	myrouter.HandleFunc("/getNextLevel", nextLevel).Methods("POST")
 	handler := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000"}, // Your React app's origin
 		AllowedMethods:   []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
@@ -396,7 +467,6 @@ func main() {
 		os.Exit(1)
 	}
 	//defer database.Connection.Close(context.Background()) // Defer the Close call here
-
 	database.Testing()
 	handleRequests()
 }
